cc22c46659bd9ba8b938573623591b25d6c49594
Ronit Jena <ronitjena64@gmail.com>
2024-01-03 13:52:23 +0530

    Setting The Voice Recognition

--- neon.py
+++ neon.py
@@ -1,1 +1,9 @@
 import pyttsx3
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+engine.setProperty('rate', 200)
+
+engine.say('hello world')
+engine.runAndWait()
